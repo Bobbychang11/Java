b61dbaf8f2434e3072168a95cf37fd1143f585c6
Bobby  Chang <bobbychang@Bobbys-MacBook-Pro.local>
2022-10-19 18:24:16 -0400

    Java codes

--- ConnectFour.java
+++ ConnectFour.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
+// Conenct Four
 public class ConnectFour {
 
   //characters for players (R for Red, Y for Yellow)
