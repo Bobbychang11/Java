43efb44d81ac1693d8790609742ec723830ed74d
Bobby  Chang <bobbychang@Bobbys-MacBook-Pro.local>
2022-10-19 18:10:35 -0400

    Java codes

--- Final Project/Shop.java
+++
@@ -1,6 +0,0 @@
-public class Shop {
- //make stack
- //push and pop
-  
-
-}
--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld{
-    public static void public static void main(String[] args) {
-        //print helloworld on screen
-        System.out.println("Hello World");
-    
-    }
-}
